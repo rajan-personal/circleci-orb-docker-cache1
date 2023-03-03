a3ca33b598d6615b8fe1f536c10ffe5e005f4c0f
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 11:56:49 +0530

    test

--- src/scripts/docker-cache.sh
+++ src/scripts/docker-cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DOCKERFILE=docker/Dockerfile
-VERSION=$(cat .python-version)
+# VERSION=$(cat .python-version)
 BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
 FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
 for ref in $FINAL_BASE_IMAGE_NAMES; do
@@ -23,7 +23,7 @@ for ref in $FINAL_BASE_IMAGE_NAMES; do
     -H "Authorization: Bearer $token" \
     -I -s "https://registry-1.docker.io/v2/${REPOSITORY}/manifests/${TAG}" | \
     grep -i ^etag: | cut -d: -f2-)
-  echo $IMAGE_DIGEST >> docker-layer-caching-key.txt
+  echo "$IMAGE_DIGEST" >> docker-layer-caching-key.txt
 done
 cp $DOCKERFILE docker-layer-caching-key.txt
 echo "$BUILD_EXCLUDE_ENVS" >> docker-layer-caching-key.txt
