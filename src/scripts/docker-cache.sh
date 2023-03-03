6742d327d14d30a13c3ec3e19671897a7fbfafc1
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 13:19:17 +0530

    cache location in variable

--- src/scripts/docker-cache.sh
+++ src/scripts/docker-cache.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # DOCKERFILE="${DOCKERFILE}"
+
+CACHE_LOCATION=/tmp/docker-layer-caching-key.txt
 VERSION=$(cat ."${LANGUAGE}"-version 2>/dev/null)
 BASE_IMAGE_NAMES=$(grep "^FROM" "$DOCKERFILE" | cut -d' ' -f2 | uniq)
 FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
 
-cp "$DOCKERFILE" docker-layer-caching-key.txt
+cp "$DOCKERFILE" $CACHE_LOCATION
 for ref in $FINAL_BASE_IMAGE_NAMES; do
   IMAGE_DIGEST=$(skopeo inspect --format '{{ .Digest }}' docker://"$ref")
-  echo "$IMAGE_DIGEST" >> docker-layer-caching-key.txt
+  echo "$IMAGE_DIGEST" >> $CACHE_LOCATION
 done
-echo "$VERSION" >> docker-layer-caching-key.txt
-echo "$BUILD_EXCLUDE_ENVS" >> docker-layer-caching-key.txt
+echo "$VERSION" >> $CACHE_LOCATION
+echo "$BUILD_EXCLUDE_ENVS" >> $CACHE_LOCATION
