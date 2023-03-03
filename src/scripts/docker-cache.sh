5c872e4c595f27c70b3055fe4f9d975d8c4df522
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 11:53:33 +0530

    test

--- src/scripts/docker-cache.sh
+++ src/scripts/docker-cache.sh
@@ -4,9 +4,9 @@ VERSION=$(cat .python-version)
 BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
 FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
 for ref in $FINAL_BASE_IMAGE_NAMES; do
-  if grep -q ':' \<<< "$ref"; then
-    REPOSITORY=$(cut -d':' -f1 \<<< "$ref")
-    TAG=$(cut -d':' -f2 \<<< "$ref")
+  if grep -q ':' \\<<< "$ref"; then
+    REPOSITORY=$(cut -d':' -f1 \\<<< "$ref")
+    TAG=$(cut -d':' -f2 \\<<< "$ref")
   else
     REPOSITORY=$ref
     TAG="latest"
