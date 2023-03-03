fb87324499fe39bcfb1143d83a2df1fbd1c16b9a
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 12:04:31 +0530

    test

--- src/scripts/docker-cache.sh
+++ src/scripts/docker-cache.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 DOCKERFILE=docker/Dockerfile
-# VERSION=$(cat .python-version)
+VERSION=$(cat .ruby-version)
 BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
 FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
+
+cp $DOCKERFILE docker-layer-caching-key.txt
 for ref in $FINAL_BASE_IMAGE_NAMES; do
-  if grep -q ':' \\<<< "$ref"; then
-    REPOSITORY=$(cut -d':' -f1 \\<<< "$ref")
-    TAG=$(cut -d':' -f2 \\<<< "$ref")
-  else
-    REPOSITORY=$ref
-    TAG="latest"
-  fi
-  if ! grep -q '/' \\<<< "$REPOSITORY"; then
-    REPOSITORY="library/$REPOSITORY"
-  fi
-  acceptM="application/vnd.docker.distribution.manifest.v2+json"
-  acceptML="application/vnd.docker.distribution.manifest.list.v2+json"
-  token=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${REPOSITORY}:pull" \
-        | jq -r '.token')
-  IMAGE_DIGEST=$(curl -H "Accept: ${acceptM}" \
-    -H "Accept: ${acceptML}" \
-    -H "Authorization: Bearer $token" \
-    -I -s "https://registry-1.docker.io/v2/${REPOSITORY}/manifests/${TAG}" | \
-    grep -i ^etag: | cut -d: -f2-)
+  IMAGE_DIGEST=$(skopeo inspect --format '{{ .Digest }}' docker://$ref)
   echo "$IMAGE_DIGEST" >> docker-layer-caching-key.txt
 done
-cp $DOCKERFILE docker-layer-caching-key.txt
+echo "$VERSION" >> docker-layer-caching-key.txt
 echo "$BUILD_EXCLUDE_ENVS" >> docker-layer-caching-key.txt
