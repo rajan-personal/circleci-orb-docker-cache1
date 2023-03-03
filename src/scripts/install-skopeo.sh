ad80e105ce3b1d560bba83872b2e1327d5588f38
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 12:24:31 +0530

    test

--- src/scripts/install-skopeo.sh
+++ src/scripts/install-skopeo.sh
@@ -1,4 +1,6 @@
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_'$(lsb_release -rs)'/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
-curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_'$(lsb_release -rs)'/Release.key | sudo apt-key add -
+SKOPEO_LINK=http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_$(lsb_release -rs)
+
+sudo sh -c "echo 'deb ${SKOPEO_LINK}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+curl -L ${SKOPEO_LINK}/Release.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install skopeo
