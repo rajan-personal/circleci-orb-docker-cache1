e95676a2ac27462d99dbc7914ec5dd8a8e5a1ad6
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 11:46:17 +0530

    test

--- src/scripts/docker-cache.sh
+++ src/scripts/docker-cache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DOCKERFILE=docker/Dockerfile
-VERSION=$(cat .python-version)
-BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
-FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
+# VERSION=$(cat .python-version)
+# BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
+# FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
 cp $DOCKERFILE docker-layer-caching-key.txt
-echo $BUILD_EXCLUDE_ENVS >> docker-layer-caching-key.txt
+echo "$BUILD_EXCLUDE_ENVS" >> docker-layer-caching-key.txt
