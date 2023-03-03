7e500b71e10483f898f0521e660f9574a5e7c061
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 08:44:15 +0530

    test

--- src/scripts/docker-cache.sh
+++ src/scripts/docker-cache.sh
@@ -15,10 +15,10 @@
 
 # DOCKERFILE="${DOCKERFILE}"
 # VERSION="${VERSION}"
-BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
+BASE_IMAGE_NAMES=$(grep "^FROM" "$DOCKERFILE" | cut -d' ' -f2 | uniq)
 FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
 
-cp $DOCKERFILE docker-layer-caching-key.txt
+cp "$DOCKERFILE" docker-layer-caching-key.txt
 for ref in $FINAL_BASE_IMAGE_NAMES; do
 IMAGE_DIGEST=$(skopeo inspect --format '{{ .Digest }}' docker://"$ref")
 echo "$IMAGE_DIGEST" >> docker-layer-caching-key.txt
