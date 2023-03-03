4027662d95014d9434768f7d17a89bdbd80a5d77
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-03 08:17:19 +0530

    test

--- src/scripts/docker-cache.sh
+++ src/scripts/docker-cache.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# DOCKERFILE=docker/Dockerfile
+# VERSION=$(cat .ruby-version)
+# BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
+# FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
+
+# cp $DOCKERFILE docker-layer-caching-key.txt
+# for ref in $FINAL_BASE_IMAGE_NAMES; do
+# IMAGE_DIGEST=$(skopeo inspect --format '{{ .Digest }}' docker://$ref)
+# echo $IMAGE_DIGEST >> docker-layer-caching-key.txt
+# done
+# echo $VERSION >> docker-layer-caching-key.txt
+# echo $BUILD_EXCLUDE_ENVS >> docker-layer-caching-key.txt
+
+
 DOCKERFILE=docker/Dockerfile
 VERSION=$(cat .ruby-version)
 BASE_IMAGE_NAMES=$(grep "^FROM" $DOCKERFILE | cut -d' ' -f2 | uniq)
@@ -6,8 +20,8 @@ FINAL_BASE_IMAGE_NAMES=$(eval echo "$BASE_IMAGE_NAMES")
 
 cp $DOCKERFILE docker-layer-caching-key.txt
 for ref in $FINAL_BASE_IMAGE_NAMES; do
-IMAGE_DIGEST=$(skopeo inspect --format '{{ .Digest }}' docker://$ref)
-echo $IMAGE_DIGEST >> docker-layer-caching-key.txt
+IMAGE_DIGEST=$(skopeo inspect --format '{{ .Digest }}' docker://"$ref")
+echo "$IMAGE_DIGEST" >> docker-layer-caching-key.txt
 done
-echo $VERSION >> docker-layer-caching-key.txt
-echo $BUILD_EXCLUDE_ENVS >> docker-layer-caching-key.txt
+echo "$VERSION" >> docker-layer-caching-key.txt
+echo "$BUILD_EXCLUDE_ENVS" >> docker-layer-caching-key.txt
