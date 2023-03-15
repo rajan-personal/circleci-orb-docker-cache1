255a2bb406478c6f329b08160ba161b07a299f82
rajan-personal <39906424+rajan-syn@users.noreply.github.com>
2023-03-15 18:20:02 +0530

    update checksum key

--- src/scripts/checksum-lock.sh
+++ src/scripts/checksum-lock.sh
@@ -2,13 +2,13 @@
 if [ -f "poetry.lock" ]; then
     checksum_python=$(md5sum poetry.lock | awk '{print $1}')
 else
-    checksum_python="none"
+    checksum_python="n"
 fi
 
 if [ -f "Gemfile.lock" ]; then
     checksum_ruby=$(md5sum Gemfile.lock | awk '{print $1}')
 else
-    checksum_ruby="none"
+    checksum_ruby="n"
 fi
 
 echo "$checksum_python-$checksum_ruby" > /tmp/checksum_key
